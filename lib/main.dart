a348722ed61463075f25f300700ee0ecaf8aede5
thomasalbertoDev <12221355@bsi.ac.id>
2024-11-08 09:57:57 +0700

    chore: add splash screen, change logo & name app

--- lib/main.dart
+++ lib/main.dart
@@ -15,48 +15,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen()
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState(){
-    super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-        if (mounted) {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => const WelcomePage()),
-          );
-        }
-      });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kasirku Mobile',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Image(
-            image: AssetImage('images/logo.png'),
-            width: 200,
-            height: 200,
-          )
-        ),
-      )
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const WelcomePage(),
+      },
     );
   }
 }
